d929cc6ab041e8c9e5dedcac0781afa82a93c885
janek_g <jmgrzesik@gmail.com>
2016-08-21 17:43:33 +0200

    Adds mongoose integration and auth/profile controllers to main

--- app.js
+++ app.js
@@ -3,6 +3,24 @@
 let express = require('express');
 let app = express();
 
-/* Your code */
+const config = require('./config/config');
+const mongoose = require('mongoose');
+const debug = require('debug')('alttab:main');
+const AuthRouter = require('./src/routers/auth.router');
+const ProfileRouter = require('./src/routers/profile.router');
+const bodyParser = require('body-parser');
+
+
+mongoose.connect(`mongodb://${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`);
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+
+app.use('/api', AuthRouter);
+app.use('/api', ProfileRouter);
+
+app.listen(config.main.port, ()=> {
+    debug(`Application listening on port ${config.main.port}`)
+});
 
 module.exports = app;
